8cd8b9e52f293409d88d813cdb295638a1093163
Vaibhav Bali <vibsbali@gmail.com>
2016-04-25 19:21:35 +1000

    Added new Greeter service and setup IOC

---
+++ src/OdeToFoodStepUp/Service/Greeter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFoodStepUp.Service
+{
+    public class Greeter : IGreeter
+    {
+        private readonly IConfiguration configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public string GetGreeting()
+        {
+            return configuration["greeting"];
+        }
+    }
+}
