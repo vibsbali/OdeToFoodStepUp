cebddc618a5dfc73e34a79a3af32e2d85a7589d8
Vaibhav Bali <vibsbali@gmail.com>
2016-04-26 13:56:26 +1000

    Completed showing a list of Restaurants

--- src/OdeToFoodStepUp/Controllers/HomeController.cs
+++ src/OdeToFoodStepUp/Controllers/HomeController.cs
@@ -1,18 +1,21 @@
 ﻿using Microsoft.AspNet.Mvc;
 using OdeToFoodStepUp.Entities;
+using OdeToFoodStepUp.Service;
 
 namespace OdeToFoodStepUp.Controllers
 {
     public class HomeController : Controller
     {
+        private IRestaurantData restaurantData;
+
+        public HomeController(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            var model = new Restaurant
-            {
-                Id = 1,
-                Name = "Sabatino's"
-            };
+            var model = restaurantData.GetAll();
 
             return View(model);
         }
