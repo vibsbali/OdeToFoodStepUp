52fa4904c286f033dba151b8f3f32fe248d9ba85
Vaibhav Bali <vibsbali@gmail.com>
2016-04-28 09:32:30 +1000

    Completed

--- src/OdeToFoodStepUp/Models/LoginViewModel.cs
+++ src/OdeToFoodStepUp/Models/LoginViewModel.cs
@@ -8,9 +8,11 @@ namespace OdeToFoodStepUp.Models
 {
     public class LoginViewModel
     {
+        [Required]
         public string Username { get; set; }
-        [DataType(DataType.Password)]
+        [DataType(DataType.Password)][Required]
         public string Password { get; set; }
+
         [Display(Name = "Remember Me")]
         public bool RememberMe  { get; set; }
 
