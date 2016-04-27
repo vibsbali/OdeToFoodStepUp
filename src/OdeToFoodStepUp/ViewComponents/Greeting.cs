9645cf1da890f91dbd14ed56a60909b1c0d19e63
Vaibhav Bali <vibsbali@gmail.com>
2016-04-27 16:19:20 +1000

    Added a view component

---
+++ src/OdeToFoodStepUp/ViewComponents/Greeting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using OdeToFoodStepUp.Service;
+
+namespace OdeToFoodStepUp.ViewComponents
+{
+    public class Greeting : ViewComponent
+    {
+        private readonly IGreeter greeter;
+
+        public Greeting(IGreeter greeter)
+        {
+            this.greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = greeter.GetGreeting();
+            return View("Default",model);
+        }
+    }
+}
